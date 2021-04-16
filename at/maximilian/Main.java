444f0cee81438796dd83360e3f16844c385bcae3
Maximilian Tamasanu <m3xpl4y@gmail.com>
2021-04-16 23:26:54 +0200

    classes created

---
+++ at/maximilian/Main.java
@@ -0,0 +1,8 @@
+package at.maximilian;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hier wird eine Fernbedienung mit Batterien konstruiert!");
+    }
+    
+}
---
+++ at/maximilian/classes/Remote.java
@@ -0,0 +1,5 @@
+package at.maximilian.classes;
+
+public class Remote {
+    
+}
