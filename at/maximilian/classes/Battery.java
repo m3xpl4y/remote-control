2ae5b00b7a563acca23439fe8d6653f7c59152e1
Maximilian Tamasanu <m3xpl4y@gmail.com>
2021-04-17 00:29:45 +0200

    classes updated

--- at/maximilian/classes/Battery.java
+++ at/maximilian/classes/Battery.java
@@ -1,5 +1,6 @@
 package at.maximilian.classes;
 
 public class Battery {
-    
+    private enum STATUS {EMPTY, LOW, FULL}
+    private STATUS status;
 }
--- at/maximilian/classes/Remote.java
+++ at/maximilian/classes/Remote.java
@@ -1,5 +1,7 @@
 package at.maximilian.classes;
 
 public class Remote {
+    private boolean isOn;
+    private boolean hasPower;
     
 }
